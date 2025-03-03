ebe0809a4a0117ba4a43f93a6d7a0d10be1ca232
s28228 <s28228@pjwstk.edu.pl>
2025-03-03 02:39:36 +0100

    Secured sensitive data

--- src/main/java/com/alexkononon/star_wars_project/StarWarsProjectApplication.java
+++ src/main/java/com/alexkononon/star_wars_project/StarWarsProjectApplication.java
@@ -1,5 +1,6 @@
 package com.alexkononon.star_wars_project;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StarWarsProjectApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("TWILIO_PHONE_NUMBER", dotenv.get("TWILIO_PHONE_NUMBER"));
+        System.setProperty("TWILIO_ACCOUNT_SID", dotenv.get("TWILIO_ACCOUNT_SID"));
+        System.setProperty("TWILIO_AUTH_TOKEN", dotenv.get("TWILIO_AUTH_TOKEN"));
+        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
         SpringApplication.run(StarWarsProjectApplication.class, args);
     }
 
