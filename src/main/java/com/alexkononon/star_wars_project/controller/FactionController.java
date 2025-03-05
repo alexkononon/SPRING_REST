e4f481f1d0fa3076d458a68816b3f42bc09ae599
s28228 <s28228@pjwstk.edu.pl>
2025-03-06 00:49:34 +0100

    GetAll() method created and some business logic

--- src/main/java/com/alexkononon/star_wars_project/controller/FactionController.java
+++ src/main/java/com/alexkononon/star_wars_project/controller/FactionController.java
@@ -1,10 +1,13 @@
 package com.alexkononon.star_wars_project.controller;
 
 import com.alexkononon.star_wars_project.dto.FactionDTO;
+import com.alexkononon.star_wars_project.dto.ResourceDTO;
 import com.alexkononon.star_wars_project.service.impl.FactionServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/factions")
@@ -26,6 +29,11 @@ public class FactionController {
         return ResponseEntity.ok(factionServiceImpl.getFaction(id));
     }
 
+    @GetMapping
+    public ResponseEntity<List<FactionDTO>> getAllFactions() {
+        return ResponseEntity.ok(factionServiceImpl.getAllFactions());
+    }
+
     @PutMapping("/{id}")
     public ResponseEntity<FactionDTO> updateFaction(@PathVariable Long id, @RequestBody FactionDTO dto) {
         return ResponseEntity.ok(factionServiceImpl.updateFaction(id, dto));
@@ -36,4 +44,19 @@ public class FactionController {
         factionServiceImpl.deleteFaction(id);
         return ResponseEntity.noContent().build();
     }
+
+    // workflow
+    //GET /api/factions/{factionId}/conflicts
+
+    @PostMapping("/{factionId}/conflict/{targetFactionId}")
+    public ResponseEntity<FactionDTO> startConflict(@PathVariable Long factionId, @PathVariable Long targetFactionId) {
+        return ResponseEntity.ok(factionServiceImpl.startConflictBetween(factionId, targetFactionId));
+    }
+
+    @GetMapping("/{factionId}/conflicts")
+    public ResponseEntity<List<FactionDTO>> getConflictingFactions(@PathVariable Long factionId) {
+        List<FactionDTO> conflicts = factionServiceImpl.getConflictingFactions(factionId);
+        return ResponseEntity.ok(conflicts);
+    }
 }
+
--- src/main/java/com/alexkononon/star_wars_project/service/impl/FactionServiceImpl.java
+++ src/main/java/com/alexkononon/star_wars_project/service/impl/FactionServiceImpl.java
@@ -10,6 +10,9 @@ import com.alexkononon.star_wars_project.service.FactionService;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 @Service
 @Transactional
 public class FactionServiceImpl implements FactionService {
@@ -18,13 +21,15 @@ public class FactionServiceImpl implements FactionService {
     private final FactionMapper factionMapper;
     private final CharacterRepository characterRepository;
     private final PlanetRepository planetRepository;
+    private final SmsServiceImpl smsServiceImpl;
 
     public FactionServiceImpl(FactionRepository factionRepository, FactionMapper factionMapper,
-                              CharacterRepository characterRepository, PlanetRepository planetRepository) {
+                              CharacterRepository characterRepository, PlanetRepository planetRepository, SmsServiceImpl smsServiceImpl) {
         this.factionRepository = factionRepository;
         this.factionMapper = factionMapper;
         this.characterRepository = characterRepository;
         this.planetRepository = planetRepository;
+        this.smsServiceImpl = smsServiceImpl;
     }
 
     public FactionDTO createFaction(FactionDTO dto) {
@@ -37,6 +42,12 @@ public class FactionServiceImpl implements FactionService {
         return factionRepository.findById(id)
                 .map(factionMapper::fromFactionToDTO)
                 .orElseThrow(() -> new RuntimeException("Faction not found with id: " + id));
+    }
+
+    public List<FactionDTO> getAllFactions() {
+        return factionRepository.findAll().stream()
+                .map(factionMapper::fromFactionToDTO)
+                .collect(Collectors.toList());
     }
 
     public FactionDTO updateFaction(Long id, FactionDTO dto) {
@@ -54,4 +65,42 @@ public class FactionServiceImpl implements FactionService {
         }
         factionRepository.deleteById(id);
     }
+
+    public FactionDTO startConflictBetween(Long factionId, Long targetFactionId) {
+        Faction faction = factionRepository.findById(factionId)
+                .orElseThrow(() -> new RuntimeException("Faction not found with id: " + factionId));
+        Faction targetFaction = factionRepository.findById(targetFactionId)
+                .orElseThrow(() -> new RuntimeException("Faction not found with id: " + targetFactionId));
+
+        if (faction.getEnemyFactions().contains(targetFaction)) {
+            throw new RuntimeException("These factions are already in conflict.");
+        }
+
+        faction.getEnemyFactions().add(targetFaction);
+
+        factionRepository.save(faction);
+
+        smsServiceImpl.sendSms("+48111111111", "The " + faction.getName() + " faction declared war on us )");
+
+        return factionMapper.fromFactionToDTO(faction);
+    }
+
+    public List<FactionDTO> getConflictingFactions(Long factionId) {
+        Faction faction = factionRepository.findById(factionId)
+                .orElseThrow(() -> new RuntimeException("Faction not found with id: " + factionId));
+
+        Set<Faction> conflicts = new HashSet<>();
+        if (faction.getEnemyFactions() != null) {
+            conflicts.addAll(faction.getEnemyFactions());
+        }
+        if (faction.getEnemyOf() != null) {
+            conflicts.addAll(faction.getEnemyOf());
+        }
+
+        return conflicts.stream()
+                .map(factionMapper::fromFactionToDTO)
+                .collect(Collectors.toList());
+    }
+
 }
+
